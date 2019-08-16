8b8b189ff010e7546b343ccf52198fa06d1e5c38
Vineet Berlia <vberlia@in.imshealth.com>
2019-08-16 22:11:01 +0530

    Added notebook for calculating convolution using for loops

---
+++ convolution.py
@@ -0,0 +1,9 @@
+import pandas
+import numpy as np
+
+x = np.array([[1, 1, 1, 0, 0],
+              [0, 1, 1, 1, 0],
+              [0, 0, 1, 1, 1],
+              [0, 0, 1, 1, 0],
+              [0, 1, 1, 0, 0]],
+             dtype='float')
